b8cc73eb2b4c45359d0f6963d3e592bf808772dc
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-07-13 18:58:53 -0400

    Started fruits module exercise

---
+++ 4-create-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import { fruits } from './foods';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fruits Module Exercise</h1>
+      
+    </div>
+  );
+}
+
+export default App;
